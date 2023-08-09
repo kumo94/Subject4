f9634baa848f30717aefc40d4c214656544c147e
th204970 <lee.chungrae@tis-h.co.jp>
2023-08-09 13:56:46 +0900

    string-array　問題

--- app/src/main/java/com/example/subject4/MyViewHolder.kt
+++ app/src/main/java/com/example/subject4/MyViewHolder.kt
@@ -9,17 +9,11 @@ class MyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
     private val checkboxId = view.findViewById<CheckBox>(R.id.checkbox)
     fun bind(viewModel: ViewModel, position:Int) {
         view.rootView.setOnClickListener {
-            // アイテムクリック時にイベントをキャッチ
-            // (今回の場合は)わざわざonItemClickListenerを呼び出す必要はない
 
-            // アイテムタップ時にタップ状態を反転させる
             viewModel.isChecked = !viewModel.isChecked
 
-            // 反転させたチェック状態をチェックボックスに反映
-            // チェックボックスの状態たちを保持しているのはViewModel
             checkboxId.isChecked = viewModel.isChecked
         }
-        // bindが呼び出されたタイミングで今現在ViewModelが持っているチェック状態を反映
         checkboxId.isChecked = viewModel.isChecked
 
         checkboxId.text = viewModel.label
